2a0aa3b8a135ff2562b85d2568085b4be60db57a
nawaf <nawaf@localhost>
2024-04-18 17:20:44 +0300

    Modify entire test class from JUnit 3.8 to JUnit 5

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,14 +1,38 @@
 package studentgrade;
 
 import junit.framework.TestCase;
-
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 /* JUnit 3.8.1 */
 public class StudentGradeTest extends TestCase {
     
     public StudentGradeTest(String testName) {
         super(testName);
     }
+    
+        @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
+    /**
+     * Test of GetGrade95 method, of class add.
+     */
+    @Test
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
@@ -17,6 +41,10 @@ public class StudentGradeTest extends TestCase {
         assertEquals(expResult, result);
     }
 
+    /**
+     * Test of GetGrade85 method, of class add.
+     */
+    @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
@@ -25,6 +53,10 @@ public class StudentGradeTest extends TestCase {
         assertEquals(expResult, result);
     }
     
+    /**
+     * Test of GetGrade75 method, of class add.
+     */
+    @Test
     public void testGetGrade75() {
         System.out.println("getGrade 85");
         int mark = 75;
